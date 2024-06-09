015a0bd47dd3be1ae6ab1fd32b5bdc0acfee36e4
Kelvin Gil <gilkelvin2@gmail.com>
2024-06-09 14:18:46 -0400

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,anagram = "hello"):
+        self.anagram = anagram
+
+    @property
+    def anagram(self):
+        return self._anagram
+    
+    @anagram.setter
+    def anagram(self,anagram):
+        self._anagram = anagram
+    
+    def match(self,anagram):
+
+        lists = []
+        listen = sorted(self.anagram)
+        for match in anagram:
+            sortd = sorted(match)
+            if sortd == listen:
+                lists.append(match)
+        return lists
